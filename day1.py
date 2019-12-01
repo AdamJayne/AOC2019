3e912aded13c2232c66fd5cf217e609994a9ff81
Adam <aoc@challenge.com>
2019-12-01 02:30:27 -0500

    day 1 complete, both stars

---
+++ day1.py
@@ -0,0 +1,12 @@
+
+with open('./puzzles/day1.txt', 'r') as f:
+    puzzle = f.read().split('\n')
+    # Part 1 Solution
+    print(sum(map(lambda mass: int(int(mass) / 3) - 2, puzzle[::])))
+
+    # Part 2 Solution
+    totalfuel = 0
+    def calc_fuel(fuel, running=0):
+        calculated = int(int(fuel) / 3) - 2
+        return calc_fuel(calculated, running+calculated) if calculated > 0 else running
+    print(sum(map(calc_fuel, puzzle[::])))
